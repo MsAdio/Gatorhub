281b132f2296deef4a13b70855263af454318a7c
Arielle Adio <78652789+MsAdio@users.noreply.github.com>
2023-04-27 02:20:35 +0000

    2023-04-26

---
+++ Test/Friends.h
@@ -0,0 +1,46 @@
+#include <vector>
+#include <string>
+
+class Friend {
+private:
+  std::string name;
+  int age;
+  std::string city;
+public:
+  Friend(std::string n, int a, std::string c) : name(n), age(a), city(c) {}
+  std::string getName() const { return name; }
+  int getAge() const { return age; }
+  std::string getCity() const { return city; }
+};
+
+class Friends {
+private:
+  std::vector<Friend> friendsList;
+public:
+  void addFriend(std::string name, int age, std::string city) {
+    Friend newFriend(name, age, city);
+    friendsList.push_back(newFriend);
+  }
+  std::vector<Friend> getFriends() const {
+    return friendsList;
+  }
+  std::vector<Friend> getFriendsInCity(std::string city) const {
+    std::vector<Friend> friendsInCity;
+    for (const Friend& friend : friendsList) {
+      if (friend.getCity() == city) {
+        friendsInCity.push_back(friend);
+      }
+    }
+    return friendsInCity;
+  }
+  std::vector<Friend> getFriendsInRange(int minAge, int maxAge) const {
+    std::vector<Friend> friendsInRange;
+    for (const Friend& friend : friendsList) {
+      int age = friend.getAge();
+      if (age >= minAge && age <= maxAge) {
+        friendsInRange.push_back(friend);
+      }
+    }
+    return friendsInRange;
+  }
+};
---
+++ Test/Manage Friends.h
@@ -0,0 +1,34 @@
+#include <vector>
+#include <string>
+
+class Friends {
+private:
+  std::vector<Friend> friendsList;
+public:
+  void addFriend(std::string name, int age, std::string city) {
+    Friend newFriend(name, age, city);
+    friendsList.push_back(newFriend);
+  }
+  std::vector<Friend> getFriends() const {
+    return friendsList;
+  }
+  std::vector<Friend> getFriendsInCity(std::string city) const {
+    std::vector<Friend> friendsInCity;
+    for (const Friend& friend : friendsList) {
+      if (friend.getCity() == city) {
+        friendsInCity.push_back(friend);
+      }
+    }
+    return friendsInCity;
+  }
+  std::vector<Friend> getFriendsInRange(int minAge, int maxAge) const {
+    std::vector<Friend> friendsInRange;
+    for (const Friend& friend : friendsList) {
+      int age = friend.getAge();
+      if (age >= minAge && age <= maxAge) {
+        friendsInRange.push_back(friend);
+      }
+    }
+    return friendsInRange;
+  }
+};
